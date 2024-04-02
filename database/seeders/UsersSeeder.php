984c3916176f75ea6b52b11c8370743dc70a7ae6
Irlan Ferreira <irlanferreiradasilva2@gmail.com>
2024-04-01 21:37:25 -0300

    .
    .

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // User::create([
+        //     "firstName"=> 'Irlan',
+        //     "lastName"=>'maneiro',
+        //     "email"=>"dka@gmail.com",
+        //     "password"=>bcrypt('knowledge')
+        // ]);
+    }
+}
---
+++ resources/views/site/home.blade.php
@@ -0,0 +1,7 @@
+@extends('site.layout')
+
+@section('titulo', 'Home')
+
+@section('conteudo')
+<h1>Conteudo</h1>
+@endsection
